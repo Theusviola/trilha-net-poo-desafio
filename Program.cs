52a42c9d31f8dddf96dea6fa31527ff57bd973dd
Matheus Santana Assunção <contato.mktmatheus21@gmail.com>
2023-12-22 17:43:39 -0300

    Implementados todos os exercícios do desafio.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // TODO: Realizar os testes com as classes Nokia e Iphone
+        Console.WriteLine("Smartphone Nokia");
+        Smartphone nokia = new Nokia(numero: "1234-5678", modelo: "N456", imei: "11111", memoria: 64);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Instagram");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Iphone");
+        Smartphone iphone = new Iphone(numero: "9123-4567", modelo: "I123", imei: "22222", memoria: 64);
+        iphone.Ligar();
+        iphone.InstalarAplicativo("Apple Music");
+    }
+}
